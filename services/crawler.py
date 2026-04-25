788084e8d0d017993b05afd652bb5e9c9d6cd34b
bebofcds <bebo.boby3@gmail.com>
2026-04-25 20:16:11 +0300

    fixed soup bugs and added title

--- services/crawler.py
+++ services/crawler.py
@@ -35,24 +35,31 @@ def BFS(start_url, max_pages=10,max_depth=4):
 
         visited.add(current_url)
 
-        data,child_title = scraper.scrape_page(res)
+        result = scraper.scrape_page(res)
+
+        if not result or len(result) != 2:
+            append_failed_links(current_url)
+            continue
+
+        data, child_title = result
         raw_children = parser.get_links(current_url, res)
 
         clean_children = []
         
-
-        for link in raw_children:
-            if link not in visited:
-                queue.append((link,depth+1))
-            clean_children.append(link)
+        if depth<max_depth:
+            for link in raw_children:
+                if link not in visited:
+                    queue.append((link,depth+1))
+                clean_children.append(link)
 
         graph[current_url] = {
             "title":child_title,
             "data": data,
             "children": clean_children,
         }
-
+    title_tag=soup.title
+    title_text=title_tag.string.strip() if title_tag and title_tag.string else ""
     return {
         "graph" : graph,
-        "title" : soup.title.string
+        "title" : title_text
     }
